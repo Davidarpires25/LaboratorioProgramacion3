91415ddf1a4642e0970b6a5154dd2e8fdbb82536
ValentinHRoldan <valentinroldan223@gmail.com>
2024-08-31 16:08:13 -0300

    fin validacion fecha y nro comprobante

--- Scripts/script_ventas.js
+++ Scripts/script_ventas.js
@@ -1,5 +1,20 @@
 const d = document;
 const $formulario = d.getElementById("ventaForm");
+
+
+function controlFecha(){
+    const $fechaSolicitud= document.getElementById("fechaVenta");
+    const fechaActual = new Date().toISOString().split('T')[0];
+    let fechaLimite = new Date();
+    $fechaSolicitud.value = fechaActual;
+    fechaLimite.setMonth(fechaLimite.getMonth() - 1)
+    fechaLimite = fechaLimite.toISOString().split('T')[0];
+
+    $fechaSolicitud.setAttribute("min",fechaLimite);
+    $fechaSolicitud.setAttribute("max",fechaActual);
+
+};
+
 function validacionNroComprobante(nroComprobante){
     const $span = d.getElementById("spanError");
     let soloNumeros = /^\d+$/;
@@ -24,4 +39,8 @@ d.addEventListener("submit", function(e){
 
 $formulario.nroComprobante.addEventListener("keydown", function(keyEvent){
     if(validarTeclas(keyEvent.keyCode)) keyEvent.preventDefault();
+})
+
+d.addEventListener("DOMContentLoaded", function(e){
+    controlFecha();
 })
