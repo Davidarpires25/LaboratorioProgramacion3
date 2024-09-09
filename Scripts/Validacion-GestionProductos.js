08dcb81041389fb44b89815345e47c36b3767ef6
Marcos Acosta <acostamarlih9@gmail.com>
2024-09-09 15:15:51 -0300

    eliminado campo ID de gestion de productos

--- Scripts/Validacion-GestionProductos.js
+++ Scripts/Validacion-GestionProductos.js
@@ -1,4 +1,3 @@
-const descripcion = document.getElementById('desc_producto');
 const botones = document.querySelectorAll('#productoForm button')
 botones.forEach(boton => {
     boton.addEventListener('click',validar)
@@ -9,29 +8,24 @@ const formulario = document.getElementById('productoForm');
 function validar(e){
     e.preventDefault();
 
-    const id = document.getElementById('id_producto');
-    if(id.value === '')
-        {
-            alert('ID de producto es obligatorio');
-            return false;
-        }
+    const descripcion = document.getElementById('desc_producto');
+    if(descripcion.value === '')
+    {
+        alert('Descripción del producto es obligatorio');
+        return false;
+
+    }else if(!validarSoloLetras(descripcion)){
+        alert('La descripcion debe ser solo letras');
+        return false;
+    }
     
     const botonPulsado = e.target.name;
-
-    prueba = 5;
 
     switch (botonPulsado) {
 
         case 'Agregar':
             if(!validarCategoria()){
                 alert('Debes seleccionar una categoria de producto');
-                return false;
-            }
-            if(!validarDescripcion()){
-                alert('Debes incluir una descripción');
-                return false;
-            }else if(!validarSoloLetras(descripcion)){
-                alert('La descripcion debe ser solo letras');
                 return false;
             }
             else if(!validarPrecio()){
@@ -44,11 +38,8 @@ function validar(e){
         break;
 
         case 'Editar':
-            if(!validarCategoria() && !validarDescripcion() && !validarPrecio() && !validarCantidad()){
+            if(!validarCategoria() && !validarPrecio() && !validarCantidad()){
                 alert('No estás modificando ningun campo');
-                return false;
-            }else if(!validarSoloLetras(descripcion)){
-                alert('La descripcion debe ser solo letras');
                 return false;
             }
         break;
@@ -72,14 +63,6 @@ function validarCategoria(){
     });
 
     if (!seleccionado) {
-        return false;
-    }
-    return true;
-}
-
-function validarDescripcion(){
-    if(descripcion.value === '')
-    {
         return false;
     }
     return true;
