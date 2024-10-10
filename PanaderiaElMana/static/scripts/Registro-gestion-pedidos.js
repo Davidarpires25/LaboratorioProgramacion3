51dab593da2e014b1d27b54a3414e913efe03495
davidarpires25 <davidarpires25@gmail.com>
2024-10-10 00:31:08 -0300

    Se actualizo el formulario de pedidos

--- PanaderiaElMana/static/scripts/Registro-gestion-pedidos.js
+++ PanaderiaElMana/static/scripts/Registro-gestion-pedidos.js
@@ -1,25 +1,63 @@
 
 
+const expresionesPedidos = {
+	observaciones: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	cantidad: /^.{1,90}$/, // 1 a 90 digitos.
+}
+const camposPedidos = {
+	observaciones: false,
+	cantidad: false,
+	
+}
 
 
-
-document.addEventListener("DOMContentLoaded", ()=>{
-    const $formulario= document.querySelector('#pedidosForm')
+document.addEventListener("DOMContentLoaded", (e)=>{
+    const $formulario= document.querySelector('#formulario')
+   
     controlFecha();
+    $formulario.addEventListener("keyup", (e) => { 
+            if(e.target.matches("#observaciones")){
+                
+                validarCampo(expresionesPedidos.observaciones, e.target, 'observaciones');
+            }   
+            else if(e.target.matches("#cantidad")){
+                validarCampo(expresionesPedidos.cantidad, e.target, 'cantidad');
+            }
+        });
 
-    $formulario.addEventListener("input",(e)=>{
-        if(e.target.matches("#cantidad")){
-            controlCantidad(e);
-
-        }   
-        else if(e.target.matches("#numeroPedido")){
-            controlPedido(e);
-        }
+   
         
+    $formulario.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        const terminos = document.getElementById('terminos');
+      
+        console.log(camposPedidos.observaciones,camposPedidos.cantidad)
+	    if(camposPedidos.observaciones && camposPedidos.cantidad ){
+            document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
+            $formulario.reset();
+            document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+            setTimeout(() => {
+                document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+            }, 5000);
+
+            document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+                icono.classList.remove('formulario__grupo-correcto');
+            });
+        }
+        else {
+            document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+            }
+            
+       
     });
+
+    
+    
  
 
 });
+
+
 
 
 function controlFecha(){
@@ -38,23 +76,25 @@ function controlFecha(){
 
 
 
-function controlCantidad(e){
-    console.log(e.target.value);
-    if(parseInt(e.target.value)<=0){               
-        e.target.setCustomValidity("La cantidad debe ser mayor que 0.");          
-    }
-    else{
-        e.target.setCustomValidity('');
-    }
-}
 
 
-function controlPedido(e){
-    if(parseInt(e.target.value)<=0){
-        e.target.setCustomValidity("Ingrese un nro de pedido valido");          
 
-    }
-    else{
-        e.target.setCustomValidity('');
-    }
+
+function validarCampo(expresion, input, campo){
+    if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		camposPedidos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		camposPedidos[campo] = false;
+	}
+    
 }
