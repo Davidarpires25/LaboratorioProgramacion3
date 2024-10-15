a38a1988ab5046f60cb964dfd39eb6dabe078020
ValentinHRoldan <valentinroldan223@gmail.com>
2024-10-15 16:17:54 -0300

    cambio nombre archivo ventas2.html a uno mas formal

--- PanaderiaElMana/apps/ventas/views.py
+++ PanaderiaElMana/apps/ventas/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def registroVentas(request):
-    return render(request, 'ventas/ventas2.html')
+    return render(request, 'ventas/Registro_gestion_ventas.html')
