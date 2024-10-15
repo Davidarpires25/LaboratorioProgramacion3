a563288245c1d1c3801d7bba4629826c8b5b4900
ValentinHRoldan <valentinroldan223@gmail.com>
2024-10-15 02:29:38 -0300

    correccion sidebar, al presionar un sidebar-link, este no volvia, se elimino los data-bs-toggle y data-bs-target, se añadio un data-collapsed

--- PanaderiaElMana/static/scripts/base.js
+++ PanaderiaElMana/static/scripts/base.js
@@ -3,3 +3,48 @@ const hamBurger = document.querySelector(".toggle-btn");
 hamBurger.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("expand");
 });
+
+document.addEventListener("click", function (e) {
+  if (e.target.matches(".sidebar-link")){
+    const parteAbajo = e.target.nextElementSibling;
+    if (parteAbajo) {
+      const colapsado = parteAbajo.classList.toggle("show");
+      e.target.dataset.collapsed = !colapsado;
+
+      if (colapsado) {
+        parteAbajo.style.height = parteAbajo.scrollHeight + "px";
+      } else {
+        parteAbajo.style.height = "0"; 
+      }
+    }
+  }
+});
+
+// document.addEventListener("click", function(e) {
+//   if (e.target.matches(".sidebar-link")) {
+//       const submenu = e.target.nextElementSibling;
+//       e.target.classList.toggle("collapsed");
+
+//       if (submenu.classList.contains("show")) {
+//           // Animación al colapsar
+//           submenu.style.height = submenu.scrollHeight + 'px';
+//           setTimeout(() => {
+//               submenu.style.height = '0';
+//           }, 1);
+//           submenu.classList.remove("show");
+//           e.target.dataset.collapsed = true;
+//       } else {
+//           // Animación al expandir
+//           submenu.classList.add("expanding");
+//           submenu.style.height = submenu.scrollHeight + 'px';
+//           submenu.classList.add("show");
+//           e.target.dataset.collapsed = false;
+
+//           // Quitar la clase "expanding" después de la transición
+//           submenu.addEventListener('transitionend', () => {
+//               submenu.classList.remove("expanding");
+//               submenu.style.height = 'auto';
+//           }, { once: true });
+//       }
+//   }
+// });
