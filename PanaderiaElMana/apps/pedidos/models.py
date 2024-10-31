3e353b8956a5c7a4daa59e701664b3175601d9a6
davidarpires25 <davidarpires25@gmail.com>
2024-10-31 00:32:24 -0300

    se organizo los estilos

--- PanaderiaElMana/apps/pedidos/models.py
+++ PanaderiaElMana/apps/pedidos/models.py
@@ -8,7 +8,7 @@ class Proveedor(models.Model):
     email= models.CharField(max_length=50)
     direccion= models.TextField(max_length=50)
     telefono= models.CharField(max_length=20)
-    estado=models.BooleanField(False)
+    estado=models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self. nombre}"
