a0a9488b8706e047a38b033fac2e85a43ebac5b6
davidarpires25 <davidarpires25@gmail.com>
2024-11-03 19:13:27 -0300

    se ajusto el base

--- PanaderiaElMana/PanaderiaElMana/urls.py
+++ PanaderiaElMana/PanaderiaElMana/urls.py
@@ -23,7 +23,7 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls,name='admin'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('', include('apps.usuarios.urls', namespace='usuarios')),
     path('pedidos/', include('apps.pedidos.urls', namespace='pedidos')),
