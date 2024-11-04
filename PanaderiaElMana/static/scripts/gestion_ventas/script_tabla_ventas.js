8e1cf8db51ed70d844a3de556eb817e4b9ebae8a
ValentinHRoldan <valentinroldan223@gmail.com>
2024-11-04 16:18:15 -0300

    PanaderiaElMana V 1.3

--- PanaderiaElMana/static/scripts/gestion_ventas/script_tabla_ventas.js
+++ PanaderiaElMana/static/scripts/gestion_ventas/script_tabla_ventas.js
@@ -42,3 +42,58 @@ function confirmCancel(enlace) {
     });
 }
 
+function buscarVenta() {
+    const inputID = document.getElementById("searchInputID").value.toUpperCase();
+    const inputDateMin = document.getElementById("searchInputDateMin").value;
+    const inputDateMax = document.getElementById("searchInputDateMax").value;
+    const tipoVenta = document.getElementById("tipoVenta_b").value.toUpperCase();
+    const estadoVenta = document.getElementById("estadoVenta_b").value.toUpperCase();
+    const table = document.querySelector(".table-container table");
+    const rows = table.getElementsByTagName("tr");
+
+    for (let i = 1; i < rows.length; i++) {
+        const idCell = rows[i].getElementsByTagName("td")[1];
+        const fechaCell = rows[i].getElementsByTagName("td")[2];
+        const tipoVentaCell = rows[i].getElementsByTagName("td")[4];
+        const estadoVentaCell = rows[i].getElementsByTagName("td")[9];
+
+        if (idCell && fechaCell && tipoVentaCell && estadoVentaCell) {
+            const idText = idCell.textContent || idCell.innerText;
+            const fechaText = fechaCell.textContent || fechaCell.innerText;
+            const fechaDate = new Date(fechaText);
+            const tipoVentaText = tipoVentaCell.textContent || tipoVentaCell.innerText;
+            const estadoVentaText = estadoVentaCell.textContent || estadoVentaCell.innerText;
+
+            let matchesID = true;
+            let matchesFecha = true;
+            let matchesTipoVenta = true;
+            let matchesEstadoVenta = true;
+            // Filtrar por ID
+            if (inputID && idText.toUpperCase().indexOf(inputID) === -1) {
+                matchesID = false;
+            }
+
+            // Filtrar por rango de fechas
+            if (inputDateMin && fechaDate < new Date(inputDateMin)) {
+                matchesFecha = false;
+            }
+            if (inputDateMax && fechaDate > new Date(inputDateMax)) {
+                matchesFecha = false;
+            }
+            // filtrar por tipo de venta
+            if (tipoVenta && tipoVentaText.toUpperCase().indexOf(tipoVenta) === -1) {
+                matchesTipoVenta = false;
+            }        
+            if (estadoVenta && estadoVentaText.toUpperCase().indexOf(estadoVenta) === -1) {
+                matchesEstadoVenta = false;
+            }              
+
+            // Mostrar o esconder la fila según las coincidencias
+            if (matchesID && matchesFecha && !matchesTipoVenta && !matchesEstadoVenta) {
+                rows[i].style.display = "";
+            } else {
+                rows[i].style.display = "none";
+            }
+        }
+    }
+}
